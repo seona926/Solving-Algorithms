072af67ceac06aabc76123d9c37c436d9796a8c9
Sophia <suna9267@gmail.com>
2021-03-30 15:11:45 +0900

    약수의 합 풀이

---
+++ programmers/약수의합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            sum += i;
+        }
+    }
+   
+    return console.log(sum);
+}
+
+solution(10);
